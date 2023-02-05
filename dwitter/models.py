2c565d82268961c303de60d09e97611e8c39b2cc
George Nyamao <gnyamao@hotmailcom>
2023-02-04 21:12:08 -0600

    Update model.py

--- dwitter/models.py
+++ dwitter/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField(
-        "self",
-        related_name="followed_by",
-        symmetrical=False,
-        blank=True
+        "self", related_name="followed_by", symmetrical=False, blank=True
     )
+
+    def __str__(self):
+        return self.user.username
