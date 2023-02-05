63959ff94eafa9d5ae48b2499b744067ec31720d
George Nyamao <gnyamao@hotmailcom>
2023-02-05 12:27:00 -0600

    Update dwitter/models.py

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
@@ -11,6 +12,7 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
@@ -19,4 +21,4 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.save()
 
 # Create a profile for each user
-post_save.connect(create_profile, sender=User)
+# Remove: post_save.connect(create_profile, sender=User)
