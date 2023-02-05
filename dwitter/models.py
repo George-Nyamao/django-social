6e51645d322e42c92109c3057326186e609ed032
George Nyamao <gnyamao@hotmailcom>
2023-02-05 07:07:17 -0600

    Add signals to model.py

--- dwitter/models.py
+++ dwitter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
@@ -9,3 +10,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+# Create a profile for each user
+post_save.connect(create_profile, sender=User)
