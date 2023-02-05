445e9031e31defce803ac2327cc9f25c5f4dbedb
George Nyamao <gnyamao@hotmailcom>
2023-02-04 20:17:37 -0600

    Update dwitter admin.py

--- dwitter/admin.py
+++ dwitter/admin.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from .models import Profile
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
 class UserAdmin(admin.ModelAdmin):
     model = User
     # Only display the "username" field
     fields = ["username"]
+    inlines = [ProfileInline]
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
-admin.site.register(Profile)
+# Remove: admin.site.register(Profile)
